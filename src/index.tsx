9fd3e466aa1174f043ec967f7240968a42c0e71f
mmubasher <mmubasher@citrusbits.com>
2022-02-03 15:22:07 +0500

    Set base to /

--- src/index.tsx
+++ src/index.tsx
@@ -11,7 +11,7 @@ import theme from './theme/theme';
 ReactDOM.render(
   <Provider store={store}>
     <React.StrictMode>
-      <BrowserRouter basename={'order'}>
+      <BrowserRouter>
         <ThemeProvider theme={theme}>
           <App />
         </ThemeProvider>
