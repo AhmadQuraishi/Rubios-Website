88a356899d630fbe0016126c33f0bde4f60b9ae5
Aqsa <aqsazahoor555@gmail.com>
2022-02-09 13:09:25 +0500

    Redux Devtool extension package added

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,13 +1,17 @@
-import { applyMiddleware, createStore } from 'redux';
+import { applyMiddleware, compose, createStore } from 'redux';
 import rootReducer from './reducers';
 import rootSaga from './sagas';
 import createSagaMiddleware from 'redux-saga';
+import { composeWithDevTools } from 'redux-devtools-extension';
 
 const sagaMiddle = createSagaMiddleware();
 
 const middleware = [sagaMiddle];
 
-const store = createStore(rootReducer, applyMiddleware(...middleware));
+const store = createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(...middleware)),
+);
 
 sagaMiddle.run(rootSaga);
 
