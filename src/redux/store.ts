0aa7b16167f49d9eb2d251b6505a525b32e4f95a
jawad1821 <muhammad.jawad@citrusbits.com>
2022-02-09 19:36:59 +0500

    redux token store

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,17 +1,13 @@
-import { applyMiddleware, compose, createStore } from 'redux';
+import { applyMiddleware, createStore } from 'redux';
 import rootReducer from './reducers';
 import rootSaga from './sagas';
 import createSagaMiddleware from 'redux-saga';
-import { composeWithDevTools } from 'redux-devtools-extension';
 
 const sagaMiddle = createSagaMiddleware();
 
 const middleware = [sagaMiddle];
 
-const store = createStore(
-  rootReducer,
-  composeWithDevTools(applyMiddleware(...middleware)),
-);
+const store = createStore(rootReducer, applyMiddleware(...middleware));
 
 sagaMiddle.run(rootSaga);
 
