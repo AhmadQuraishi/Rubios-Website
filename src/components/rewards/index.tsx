65f77808606edfc76dd7cd970e9af76afd7abe28
Muneeb <muneeb.farooq@citrusbits.com>
2022-02-16 22:21:26 +0500

    RW-38 (Checkout UI)

--- src/components/rewards/index.tsx
+++ src/components/rewards/index.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Button, Card, Grid, RadioGroup, Typography } from '@mui/material';
+import { Button, Card, Grid, RadioGroup, Typography, ToggleButtonGroup, ToggleButton } from '@mui/material';
 import drinks from '../../assets/imgs/drinks.svg';
 import food from '../../assets/imgs/food.svg';
 import FormControlLabel from '@mui/material/FormControlLabel';
@@ -22,6 +22,15 @@ const Rewards = () => {
   const handler = () => {
     setView(!view);
   };
+
+  const [alignment, setAlignment] = React.useState('web');
+  const onRewardSelect = (
+    event: React.MouseEvent<HTMLElement>,
+    newAlignment: string,
+  ) => {
+    setAlignment(newAlignment);
+  };
+
   return (
     <Grid container>
       <Grid item xs={1} sm={0.5} md={0.5} lg={2} />
@@ -37,27 +46,42 @@ const Rewards = () => {
       )}
 
       {!view && (
-        <Grid item xs={10} sm={11} md={11} lg={8}>
+        <Grid item xs={10} sm={11} md={11} lg={8} className="choose-reward">
           <Typography variant="h4" title="APPLY REWARDS">
             APPLY REWARDS
           </Typography>
+          <br/>
           <Grid container>
             <FormControl>
-              <RadioGroup row aria-labelledby="rewards" name="Rubio,s rewards">
+              {/*<RadioGroup row aria-labelledby="rewards" name="Rubio,s rewards">*/}
+                {/*{rewardsArray.map((reward, index) => (*/}
+                  {/*<Grid item xs={12} sm={12} md={6} key={reward.desc + index}>*/}
+                    {/*<Card>*/}
+                      {/*<FormControlLabel*/}
+                        {/*value={reward.desc}*/}
+                        {/*control={<Radio />}*/}
+                        {/*label={reward.desc}*/}
+                        {/*aria-label={reward.desc}*/}
+                        {/*title={reward.desc}*/}
+                      {/*/>*/}
+                      {/*<img src={require('../../assets/imgs/taco-original-fish.jpg')} />*/}
+                    {/*</Card>*/}
+                  {/*</Grid>*/}
+                {/*))}*/}
+              {/*</RadioGroup>*/}
+              <ToggleButtonGroup
+                value={alignment}
+                exclusive
+                onChange={onRewardSelect}
+                aria-labelledby="rewards"
+              >
                 {rewardsArray.map((reward, index) => (
-                  <Grid item xs={12} sm={12} md={6} key={reward.desc + index}>
-                    <Card>
-                      <FormControlLabel
-                        value={reward.desc}
-                        control={<Radio />}
-                        label={reward.desc}
-                        aria-label={reward.desc}
-                        title={reward.desc}
-                      />
-                    </Card>
-                  </Grid>
+                    <ToggleButton value={reward.desc} className="choose-btn">
+                      <img src={reward.icon} />
+                      <Typography>{reward.desc}</Typography>
+                    </ToggleButton>
                 ))}
-              </RadioGroup>
+              </ToggleButtonGroup>
             </FormControl>
           </Grid>
         </Grid>
