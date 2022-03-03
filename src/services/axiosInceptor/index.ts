81bfdfcc6e96f0e7d1d2a1afe772b3d8fe5cf57f
Aqsa <aqsazahoor555@gmail.com>
2022-03-03 18:41:27 +0500

    Signature

--- src/services/axiosInceptor/index.ts
+++ src/services/axiosInceptor/index.ts
@@ -4,16 +4,16 @@ import * as CryptoJS from "crypto-js";
 const axiosInstance = axios.create();
 axiosInstance.interceptors.request.use(
     function (config) {
-      try {
+    try {
         const url = config.url || '';
         let origin = window.location.origin; // http://localhost:3000
         let check = url?.toString().includes('punchh_api');
-        let preppedUrl = origin + url.replace('/punchh_api/', '/');
+        let preppedUrl = url.replace('/punchh_api/', '/');
+
         if (check) {
           var uri = new URL(preppedUrl?.toString());
           const body = config.data;
           let uriData = uri.pathname.concat(uri.search);
-          // var uriData = uri.toString().replace(origin,'');
           let secret = process.env.REACT_APP_PUNCHH_CLIENT_SECRET || '';
           let secretString = secret.toString();
           let concatString = '';
@@ -23,8 +23,8 @@ axiosInstance.interceptors.request.use(
           else{
             concatString = uriData.concat(JSON.stringify(body));
           }
-          const signature = CryptoJS.HmacSHA1(concatString,secretString).toString();
-      config.headers = {'x-pch-digest': signature };
+          const signature = CryptoJS.HmacSHA1(concatString, secretString).toString();
+          config.headers = {'x-pch-digest': signature };
         }
         return config;
       } catch (e) {
