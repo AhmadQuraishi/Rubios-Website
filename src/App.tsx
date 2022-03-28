470f2b00812b2243fd6d293f4901855db4f45f8f
Muneeb <muneeb.farooq@citrusbits.com>
2022-03-28 12:37:05 +0500

    RW-301

--- src/App.tsx
+++ src/App.tsx
@@ -54,7 +54,7 @@ function App(props: any) {
               <Grid
                 item
                 xs={0}
-                sm={3}
+                sm={3.5}
                 lg={2.5}
                 sx={{ display: { xs: 'none', sm: 'grid' } }}
               >
@@ -63,7 +63,7 @@ function App(props: any) {
               <Grid
                 item
                 xs={12}
-                sm={9}
+                sm={8.5}
                 lg={9}
                 sx={{ padding: { xs: '30px 20px', sm: '30px 40px' } }}
               >
