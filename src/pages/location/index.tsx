fba762b4fb0652b26dd12c686790f5cceaa3586e
aleembaig388 <58807728+aleembaig388@users.noreply.github.com>
2022-04-19 01:12:15 +0500

    Update index.tsx

--- src/pages/location/index.tsx
+++ src/pages/location/index.tsx
@@ -1,5 +1,11 @@
-import { useEffect, useState } from 'react';
-import { GoogleMap, LoadScript, Marker } from '@react-google-maps/api';
+
+import { useCallback, useEffect, useRef, useState } from 'react';
+import {
+  GoogleMap,
+  LoadScript,
+  Marker,
+  useLoadScript,
+} from '@react-google-maps/api';
 import LocationCard from '../../components/location';
 import { useDispatch, useSelector } from 'react-redux';
 import { ResponseRestaurant } from '../../types/olo-api';
@@ -33,11 +39,24 @@ const useStyles = makeStyles((theme: Theme) => ({
   },
 }));
 
+const mapContainerStyle = {
+  width: '100%',
+  height: 'auto',
+};
 const Location = () => {
+  const { isLoaded, loadError } = useLoadScript({
+    googleMapsApiKey: process.env.REACT_APP_GOOGLE_API_KEY?.toString() || '',
+    libraries: ['places'],
+  });
   const classes = useStyles();
   const { restaurants, loading } = useSelector(
     (state: any) => state.restaurantListReducer,
   );
+
+  const mapRef = useRef();
+  const loadMap = useCallback((map: any) => {
+    mapRef.current = map;
+  }, []);
 
   const [mapCenter, setMapCenter] = useState<any>();
   const [showNearBy, setShowNearBy] = useState(false);
@@ -103,7 +122,6 @@ const Location = () => {
       );
     }
   }, [showNearBy, LatLng]);
-
 
   useEffect(() => {
     if (restaurants && restaurants.restaurants) {
@@ -189,61 +207,64 @@ const Location = () => {
         };
         setMarkers((markers) => [
           ...markers,
-          <Marker key={Math.random() + index} position={latLong} />,
+          <Marker
+            key={Math.random() + index}
+            position={latLong}
+            // icon={{
+            //   url: '/marker.png',
+            //   scaledSize: new google.maps.Size(40, 40),
+            // }}
+          />,
         ]);
       });
     }
   }, [restaurants]);
 
   return (
-    <div
-      style={{ minHeight: '300px', position: 'relative' }}
-      role="region"
-      aria-label="map"
-    >
-      {loading && (
+    <div style={{ minHeight: '300px', position: 'relative' }}>
+      {(loading || window.google == undefined || actionPerform) && (
         <div className={classes.dummyBg}>
           <LoadingBar />
         </div>
       )}
-      <LoadScript
-        googleMapsApiKey={
-          process.env.REACT_APP_GOOGLE_API_KEY?.toString() || ''
-        }
-        libraries={['places']}
-      >
-        <GoogleMap
-          center={mapCenter}
-          zoom={zoom}
-          options={{
-            streetViewControl: false,
-            mapTypeControl: false,
-            fullscreenControl: false,
-          }}
-        >
-          <div
-            onClick={() => {
-              setMayLocation();
+      {window.google && (
+        <div role="region" aria-label="map">
+          <GoogleMap
+            mapContainerStyle={mapContainerStyle}
+            zoom={zoom}
+            center={mapCenter}
+            options={{
+              streetViewControl: false,
+              mapTypeControl: false,
+              fullscreenControl: false,
             }}
-            className="location-icon-panel"
+            onLoad={loadMap}
           >
-            <span className="icon"></span>
-          </div>
-          {markers}
-          <LocationCard
-             isNearByRestaurantList={nearByRestaurantsFound}
-             restaurants={allResturants}
-             deliveryRasturants={deliveryRasturants}
-             setOrderTypeMain={setOrderType}
-             setShowNearBy={setShowNearBy}
-             setLatLng={setLatLng}
-             setActionPerform={setActionPerform}
-             setDeliveryRasturants={setDeliveryRasturants}
-          />
-        </GoogleMap>
-      </LoadScript>
+            {markers}
+            <div
+              onClick={() => {
+                setMayLocation();
+              }}
+              className="location-icon-panel"
+            >
+              <span className="icon"></span>
+            </div>
+            <LocationCard
+              isNearByRestaurantList={nearByRestaurantsFound}
+              restaurants={allResturants}
+              deliveryRasturants={deliveryRasturants}
+              setOrderTypeMain={setOrderType}
+              setShowNearBy={setShowNearBy}
+              setLatLng={setLatLng}
+              setActionPerform={setActionPerform}
+              setDeliveryRasturants={setDeliveryRasturants}
+            />
+          </GoogleMap>
+        </div>
+      )}
     </div>
   );
 };
 
 export default Location;
+
