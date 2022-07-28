37950c881720b7b1b9ed1e592345b32b1c454c44
Asmar03 <asmar.munir@citrusbits.com>
2022-07-28 20:26:47 +0500

    update changes

--- src/components/product-skeleton-ui/index.tsx
+++ src/components/product-skeleton-ui/index.tsx
@@ -2,6 +2,7 @@ import { Grid, Skeleton } from '@mui/material';
 
 const ProductSkeletonUI = () => {
   return (
+    <>
     <Grid
       container
       sx={{
@@ -41,6 +42,67 @@ const ProductSkeletonUI = () => {
         />
       </Grid>
     </Grid>
+    <Grid
+    container
+      sx={{
+        padding: { xs: '30px 20px', sm: '40px 30px' },
+        maxWidth: '1200px',
+        width: { xs: '100%', sm: '80% !important' },
+        margin: 'auto',
+      }}>
+        <Grid item xs={12} lg={6} sx={{ padding: '0px !important' }}>
+        <Skeleton variant="rectangular" sx={{ width: '60%' }} height="20px" />
+        </Grid>
+        <Grid container spacing={3} style={{marginTop: '10px'}}>
+           <Grid item xs={6}  lg={4}>
+           <Skeleton variant="rectangular" width = '100%'  height="100px" />
+           </Grid>
+           <Grid item xs={6} lg={4}>
+           <Skeleton variant="rectangular" width = '100%'  height="100px" />
+           </Grid> 
+           <Grid item xs={6} lg={4}>
+           <Skeleton variant="rectangular"  width = '100%'  height="100px" />
+           </Grid>
+           <Grid item xs={6} lg={4}>
+           <Skeleton variant="rectangular"  width = '100%'  height="100px" />
+           </Grid>
+        </Grid>
+        <Grid item xs={12} lg={6} sx={{ padding: '0px !important', marginTop: '40px' }}>
+        <Skeleton variant="rectangular" sx={{ width: '60%' }} height="20px" />
+        </Grid>
+        <Grid container spacing={3} style={{marginTop: '10px'}}>
+           <Grid item xs={6}  lg={4}>
+           <Skeleton variant="rectangular" width = '100%'  height="100px" />
+           </Grid>
+           <Grid item xs={6} lg={4}>
+           <Skeleton variant="rectangular" width = '100%'  height="100px" />
+           </Grid> 
+           <Grid item xs={6} lg={4}>
+           <Skeleton variant="rectangular"  width = '100%'  height="100px" />
+           </Grid>
+           <Grid item xs={6} lg={4}>
+           <Skeleton variant="rectangular"  width = '100%'  height="100px" />
+           </Grid>
+        </Grid>
+        <Grid item xs={12} lg={6} sx={{ padding: '0px !important', marginTop: '40px' }}>
+        <Skeleton variant="rectangular" sx={{ width: '60%' }} height="20px" />
+        </Grid>
+        <Grid container spacing={3} style={{marginTop: '10px'}}>
+           <Grid item xs={6}  lg={4}>
+           <Skeleton variant="rectangular" width = '100%'  height="100px" />
+           </Grid>
+           <Grid item xs={6} lg={4}>
+           <Skeleton variant="rectangular" width = '100%'  height="100px" />
+           </Grid> 
+           <Grid item xs={6} lg={4}>
+           <Skeleton variant="rectangular"  width = '100%'  height="100px" />
+           </Grid>
+           <Grid item xs={6} lg={4}>
+           <Skeleton variant="rectangular"  width = '100%'  height="100px" />
+           </Grid>
+        </Grid>
+    </Grid>
+    </>
   );
 };
 
