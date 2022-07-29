948b7cdd9f77225dcd32be471ba321149f49ea6a
Asmar03 <asmar.munir@citrusbits.com>
2022-07-29 20:41:36 +0500

    remove toast on mobile screens

--- src/redux/actions/create-fave/index.ts
+++ src/redux/actions/create-fave/index.ts
@@ -2,6 +2,11 @@ import { displayToast } from '../../../helpers/toast';
 import { ResponseUserFaves } from '../../../types/olo-api';
 import { createFaveTypes as Type } from '../../types/create-fave';
 
+const breakpoints = {
+  XS: 540
+};
+
+
 export function createFave(body: RequestNewFave) {
   return {
     type: Type.CREATE_FAVE,
@@ -10,7 +15,9 @@ export function createFave(body: RequestNewFave) {
 }
 
 export function createFaveSuccess(data: ResponseUserFaves) {
+   if(!breakpoints.XS){
   displayToast('SUCCESS', 'Order saved as favorite');
+   }
   return {
     type: Type.CREATE_FAVE_SUCCESS,
     payload: data,
