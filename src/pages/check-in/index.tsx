60566538d01890b690958dd9e2ab23704a1d1006
Mubashircb <mubashir.nazir@citrusbits.com>
2022-09-04 21:55:03 +0500

    RW-831

--- src/pages/check-in/index.tsx
+++ src/pages/check-in/index.tsx
@@ -1,5 +1,6 @@
 import { Grid, Box, Button, TextField, Typography, Theme } from '@mui/material';
-import receipt from '../../assets/imgs/receipt.png';
+import receipt from '../../assets/imgs/receipt_rewards.png';
+import qrCode from '../../assets/imgs/qr_code_rewards.png';
 import { Formik } from 'formik';
 import * as Yup from 'yup';
 import './check-in.css';
@@ -10,16 +11,21 @@ import { IMaskInput } from 'react-imask';
 import { forwardRef } from 'react';
 const useStyles = makeStyles((theme: Theme) => ({
   root: {
-    padding: '0px 20px 40px 20px',
+    padding: '0px 20px 0px 20px',
     maxWidth: '1260px',
     boxSizing: 'border-box',
     margin: 'auto',
   },
   heading: {
     paddingBottom: '0px',
+    fontSize: '30px !important',
     [theme.breakpoints.down('sm')]: {
       fontSize: '25px !important',
     },
+  },
+  input: {
+    paddingTop: 12,
+    paddingBottom: 12,
   },
 }));
 
@@ -53,11 +59,40 @@ const CheckIn = () => {
   const dispatch = useDispatch();
   return (
     <div className={classes.root}>
-      <Typography className={classes.heading} variant="h1" title="CHECK-IN">
-        CHECK-IN
-      </Typography>
       <Grid container>
-        <Grid item md={10}>
+        <Grid item xs={12} md={6}>
+          <Typography className={classes.heading} variant="h1" title="CHECK-IN">
+            CHECK IN
+          </Typography>
+          <Typography
+            variant="body2"
+            className="body-text"
+            title="To check in at the restaurant, scan the QR code below at the register."
+          >
+            To check in at the restaurant, scan the QR code below at the
+            register.
+          </Typography>
+          <Typography
+            variant="body2"
+            className="body-text"
+            title="If you order online, you automatically checked in."
+          >
+            If you order online, you automatically checked in.
+          </Typography>
+          <div style={{ textAlign: 'center' }}>
+            <Box
+              component="img"
+              alt="QR Code"
+              style={{ paddingTop: 25, paddingBottom: 50 }}
+              aria-label="QR Code"
+              src={qrCode}
+            />
+          </div>
+        </Grid>
+        <Grid item md={12}>
+          <Typography className={classes.heading} variant="h1" title="CHECK-IN">
+            FORGOT TO CHECK IN?
+          </Typography>
           <Formik
             initialValues={{
               barcode: '',
@@ -83,16 +118,25 @@ const CheckIn = () => {
             }) => (
               <form onSubmit={handleSubmit}>
                 <Grid container className="check-in-section">
-                  <Grid item xs={12} md={5}>
+                  <Grid item xs={12} md={6}>
                     <Grid container>
                       <Typography
                         variant="body2"
                         className="body-text"
-                        title="To check-in for your visit, enter the Rubio's Rewards barcode
-              number located at the bottom of your receipt."
+                        title="If you did not check in at the restaurant, you may enter the Rubio’s Rewards barcode number located at the bottom of your receipt."
                       >
-                        To check-in for your visit, enter the Rubio's Rewards
-                        barcode number located at the bottom of your receipt.
+                        If you did not check in at the restaurant, you may enter
+                        the Rubio’s Rewards barcode number located at the bottom
+                        of your receipt.
+                      </Typography>
+
+                      <Typography
+                        variant="body2"
+                        className="body-text"
+                        title="Receipt codes must be scanned or manually entered within 48 hours of purchase."
+                      >
+                        Receipt codes must be scanned or manually entered within
+                        48 hours of purchase.
                       </Typography>
                     </Grid>
                     <Grid
@@ -109,7 +153,11 @@ const CheckIn = () => {
                         title="Receipt Image"
                       />
                     </Grid>
-                    <Grid item xs={12}>
+                    <Grid
+                      style={{ paddingTop: 30, paddingBottom: 10 }}
+                      item
+                      xs={12}
+                    >
                       <TextField
                         aria-label="Rubio's rewards barcode number"
                         name="barcode"
@@ -125,28 +173,36 @@ const CheckIn = () => {
                           inputComponent: NumberFormatCustom as any,
                         }}
                         sx={{ width: '100%' }}
+                        className="checkin-field"
                       />
                     </Grid>
                     {/*<Grid item xs={12}>*/}
-                      {/*<Typography*/}
-                        {/*variant="body2"*/}
-                        {/*className="body-text"*/}
-                        {/*title="Please enter the 12 or 13 digits numeric barcode at the bottom of*/}
-              {/*your receipt."*/}
-                      {/*>*/}
-                        {/*Please enter the 12 or 13 digit numeric barcode at the*/}
-                        {/*bottom of your receipt.*/}
-                      {/*</Typography>*/}
+                    {/*<Typography*/}
+                    {/*variant="body2"*/}
+                    {/*className="body-text"*/}
+                    {/*title="Please enter the 12 or 13 digits numeric barcode at the bottom of*/}
+                    {/*your receipt."*/}
+                    {/*>*/}
+                    {/*Please enter the 12 or 13 digit numeric barcode at the*/}
+                    {/*bottom of your receipt.*/}
+                    {/*</Typography>*/}
                     {/*</Grid>*/}
                     <Grid item xs={12}>
                       <Typography
                         variant="body2"
                         className="body-text"
-                        title="Please ensure that you enter any leading zeros that may appear in
-              the barcode e.g 0600101234124."
+                        title="Please enter the 12 or 13 digit numeric barcode at the bottom of your receipt."
                       >
-                        Please ensure that you enter any leading zeros that may
-                        appear in the barcode e.g 0600101234124.
+                        Please enter the 12 or 13 digit numeric barcode at the
+                        bottom of your receipt.
+                      </Typography>
+                      <Typography
+                        variant="body2"
+                        className="body-text"
+                        title="Please ensure that you enter any leading zeroes that may appear in the barcode. e.g. 0600101234124"
+                      >
+                        Please ensure that you enter any leading zeroes that may
+                        appear in the barcode. e.g. 0600101234124
                       </Typography>
                     </Grid>
                     <Grid item xs={12} sm={8} md={10} lg={10}>
@@ -155,6 +211,7 @@ const CheckIn = () => {
                         title="SUBMIT"
                         type="submit"
                         variant="contained"
+                        style={{ marginTop: 20 }}
                         sx={{ width: '100%' }}
                       >
                         SUBMIT
