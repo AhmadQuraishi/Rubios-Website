78dc45164864a1eeade423a710c03cce037ff561
tabishhassan5 <tabish.hassan@citrusbits.com>
2023-02-27 18:20:02 +0500

    Fixes

--- src/MainApp.tsx
+++ src/MainApp.tsx
@@ -110,7 +110,7 @@ function App(props: any) {
         console.log("working3", restaurantSessionTime);
         const minutes = currentTime.diff(restaurantSessionTime, 'minutes');
         console.log(minutes, "minutes")
-        if (minutes > 5) {
+        if (minutes > 180) {
           dispatch(resetRestaurantRequest());
           dispatch(resetBasketRequest());
           // setOpen(true);
